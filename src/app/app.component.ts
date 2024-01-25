04f7a5a5c1c99008ca18b6112d0b278ebe6de43a
Lolo <lolo.test@test.com>
2024-01-25 15:00:05 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'Angular_Pomodoro';
+  seconds: number = 0;
+  minutes: number = 0;
+  hour: number = 0;
+  start: string = "Start";
+  isPaused: boolean = true;
+  IntervalId:any
+
+  Start() {
+    if (this.isPaused) {
+      this.isPaused = false;
+      this.start = "Pause";
+
+     this.IntervalId= setInterval(() => {
+        if (!this.isPaused) {
+          this.seconds++;
+          if (this.seconds === 60) {
+            this.minutes++;
+            this.seconds = 0;
+          }
+          if (this.minutes === 60) {
+            this.hour++;
+            this.minutes = 0;
+          }
+        }
+      }, 1000);
+    } else {
+      this.isPaused = true;
+      this.start = "Start";
+      clearInterval(this.IntervalId)
+    }
+  }
 }
